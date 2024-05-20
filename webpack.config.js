a0968fee2f32ae982f59c2d56e40373ac6775765
VadzimAJ <vadzim.anishchanka.j@gmail.com>
2024-05-21 01:17:26 +0300

    feat: configured webpack and project structure

---
+++ webpack.config.js
@@ -0,0 +1,40 @@
+const path = require('path');
+
+module.exports = {
+  mode: 'development', // Или 'production' в зависимости от окружения
+  entry: './src/sass/main.scss',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.scss$/,
+        use: [
+          'style-loader',
+          'css-loader',
+          'sass-loader'
+        ]
+      },
+      {
+        test: /\.css$/,
+        use: [
+          'style-loader',
+          'css-loader'
+        ]
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              outputPath: 'fonts',
+            }
+          }
+        ]
+      }
+    ]
+  }
+};
